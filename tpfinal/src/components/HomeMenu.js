809892fb936a4ba9df6aa834c3e295ac5db7d946
bautipeleritti <bpeleritti@udesa.edu.ar>
2024-11-11 18:18:31 -0300

    estructura de archivos

---
+++ tpfinal/src/components/HomeMenu.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native-web'
+
+export class HomeMenu extends Component {
+  render() {
+    return (
+      <div>HomeMenu</div>
+    )
+  }
+}
+
+export default HomeMenu
